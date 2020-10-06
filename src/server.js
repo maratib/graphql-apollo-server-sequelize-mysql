4a52d50c54b451a0818096b48dc35edbb5df7a0b
maratib <maratib@maratib.local>
2020-10-06 06:57:42 +0500

    server update reverted

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ const typeDefs = require('./typedefs')
 const resolvers = require('./resolvers')
 const models = require('../models')
 
+function cb(req) {
+	console.log('Here we are');
+	req.next();
 
-const server = new ApolloServer({ typeDefs, resolvers, context: {models} })
+}
+
+const server = new ApolloServer({
+	typeDefs, resolvers, context: { models }
+})
 
 server.listen().then((url) => {
 	console.log(`🚀  Server ready at ${url.url}`);
