3ae0307f5ca68ec24d1fbd595e296976fb0b1441
maratib <maratib@maratib.local>
2020-10-06 05:52:24 +0500

    unique email added

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
   };
   User.init({
     name: { type: DataTypes.STRING, allowNull: false},
-    email: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false }
   }, {
     sequelize,
